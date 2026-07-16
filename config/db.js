626f11a01cbd65d5e06b248222a8504565c7d679
ramanjeet mandal <ramanjeetmandal4619@gamil.com>
2026-07-16 19:10:18 +0530

    Debug MongoDB URI

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("Mongo URI:", process.env.MONGODB_URI);
+
     await mongoose.connect(process.env.MONGODB_URI);
+
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.log("❌ MongoDB Connection Error:", error.message);
